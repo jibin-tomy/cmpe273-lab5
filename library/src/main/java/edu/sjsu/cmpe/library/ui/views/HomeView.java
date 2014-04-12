bf57eacc27a1961c19ff883c8dd1a2b4b9e3e9ae
jibintomy <jibintomy@gamil.com>
2014-04-12 08:38:10 -0700

    first commit

--- library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
+++ library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
@@ -1,10 +1,12 @@
 package edu.sjsu.cmpe.library.ui.views;
 
+import java.util.List;
+
 import com.yammer.dropwizard.views.View;
 
 import edu.sjsu.cmpe.library.domain.Book;
 
-public class HomeView extends View {
+/*public class HomeView extends View {
     private final Book book;
 
     public HomeView(Book book) {
@@ -14,5 +16,16 @@ public class HomeView extends View {
 
     public Book getBook() {
 	return book;
+    }
+}*/
+
+public class HomeView extends View {
+    private final List<Book> books;
+    public HomeView(List<Book> books) {
+              super("home.mustache");
+              this.books = books;
+    }
+    public List<Book> getBooks() {
+              return books;
     }
 }
